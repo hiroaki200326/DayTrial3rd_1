276ec98d30615fa78f9ce1298178c20e3352b2bb
hiro200326 <h.papatuber1907@gmail.com>
2020-06-12 23:58:10 +0900

    【デイトラ3rd_2】 関数とif文

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <h1>デイトラ！</h1>
     <h2>
       <?php
+        // DAY1
         echo "Hello world!\n";
         echo "<br>";
         echo "あはははは　ははは";
@@ -24,7 +25,30 @@
         echo 3/2;
         echo "<br>";
         echo 3%2;
+        echo "<br>";
 
+        // DAY2
+        $score = 50;
+        echo $score;
+
+        echo "<br>";
+
+        function calCircleArea($radius) {
+          $pai = 3.14;
+          $area = $radius * $radius * $pai;
+          return $area;
+        }
+        echo calCircleArea(6);
+
+        echo "<br>";
+
+        if ($score > 79){
+          echo "すごい！よく頑張ったね！";
+        } elseif ($score > 60){
+          echo "あと少しで70点！頑張ろう！";
+        } else{
+          echo "もっと高得点を狙えるはず！引き続き頑張ろう！";
+        }
       ?>
     </h2>
 </body>
