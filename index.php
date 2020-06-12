c26a3f1c19661b293039535f709e1b6f49fd8619
hiro200326 <h.papatuber1907@gmail.com>
2020-06-12 23:42:43 +0900

    【デイトラ3rd_1】 echoで文字列表示

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>デイトラ PHP</title>
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+    <h1>デイトラ！</h1>
+    <h2>
+      <?php
+        echo "Hello world!\n";
+        echo "<br>";
+        echo "あはははは　ははは";
+        echo "<br>";
+        echo 1+2;
+        echo "<br>";
+        echo 10-4;
+        echo "<br>";
+        echo 5*2;
+        echo "<br>";
+        echo 3/2;
+        echo "<br>";
+        echo 3%2;
+
+      ?>
+    </h2>
+</body>
+</html>
